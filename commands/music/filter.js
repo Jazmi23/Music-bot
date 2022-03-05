c1b571319dcce829a762bcdc1140e7481b93e741
Jazmi23 <100109700+Jazmi23@users.noreply.github.com>
2022-03-06 05:27:50 +0800

    Update filter.js

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'filter',
     aliases: [],
-    utilisation: '{prefix}filter [filter name]',
+    utilisation: '{prefix}f [filter name]',
     voiceChannel: true,
 
     async execute(client, message, args) {
