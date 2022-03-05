24474ae1aa5949269cab0271e9728c0e6f637cd3
Jazmi23 <100109700+Jazmi23@users.noreply.github.com>
2022-03-06 05:48:41 +0800

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -10,6 +10,7 @@ const Embed = new MessageEmbed()
 	.setColor('#0099ff');
 	.setAuthor({ name: 'BG MUSIC', iconURL: 'https://imgur.com/ZokD85J'});
 	.setDescription('Music is your own experience, your thoughts, your wisdom. If you don’t live it, it won’t come out of your horn.');
+ 	.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 	.setTimestamp();
 	.setFooter({text: 'Some footer text here', iconURL: 'https://imgur.com/ZokD85J'});
 
